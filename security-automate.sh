d1569fc1b7e77276e188c6b6abc7b72feae8ff51
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-07-19 17:15:02 -0400

    Update security-automate.sh

--- security-automate.sh
+++ security-automate.sh
@@ -7,47 +7,62 @@
 #iptoallow="192.168.1.1" # [ OPTIONAL ]
 #ssh_pbkey="PubkeyAuthentication yes" # [ OPTIONAL ]
 #ssh_passwdauth="PasswordAuthentication yes" # [ OPTIONAL ] 
-ssh_include="Include /etc/ssh/sshd_config.d/*.conf"
 ssh_port="Port 22"
 ssh_permitroot="PermitRootLogin no"
 ssh_kbd="KbdInteractiveAuthentication no"
 ssh_pam="UsePAM yes"
 ssh_x11="X11Forwarding yes"
-ssh_motd="PrintMotd no"
-ssh_env="AcceptEnv LANG LC_*"
-ssh_sub="Subsystem sftp /usr/lib/openssh/sftp-server"
-ssh_configbackup="sshd_config_backup" #backup up file name from sshd_config
+
+#/etc/login.defs
+umasknum="027"
+minrounds="SHA_CRYPT_MIN_ROUNDS 10000"
+maxrounds="SHA_CRYPT_MAX_ROUNDS 20000"
 
 #FAIL2BAN SSHD
 enabled="true"
 findtime="48h"
 bantime="48h"
 maxretry="3"
-fail2banconfig="sshd.conf"
 
 #UFW
 #example = ufw_command="ufw limit 22 ; ufw deny 80 ; ufw allow 443"
 ufw_command="ufw limit 22"
 
 
+
 #!!!!!!!!!!!!!!NOT intended to be changed pass this line!!!!!!!!!!!!!!!!!
 
+#copy default configs to exsisting configs
+cp defaults/sshd_config /etc/ssh/sshd_config
+cp defaults/login.defs /etc/login.defs
 
-#ssh variables added to new config file
-ssh_config="sshd_config_new" #new config file name to replace sshd_config
-cd /etc/ssh && cp sshd_config $ssh_configbackup && touch $ssh_config || rm $ssh_config
-echo $ssh_include >> $ssh_config
-echo $ssh_port >> $ssh_config
-echo $ssh_permitroot >> $ssh_config
-echo $ssh_kbd >> $ssh_config
-echo $ssh_pam >> $ssh_config
-echo $ssh_x11 >> $ssh_config
-echo $ssh_motd >> $ssh_config
-echo $ssh_env >> $ssh_config
-echo $ssh_sub >> $ssh_config
-echo $ssh_pbkey >> $ssh_config
-echo $ssh_passwdauth >> $ssh_config
-mv $ssh_config sshd_config
+#file variables
+sshconfignew="sshd_config_new"
+sshconfig="sshd_config"
+cd /etc/ssh
+
+#removing lines containing patterns from default file
+grep -v "Port" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "PermitRootLogin" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "KbdInteractiveAuthentication" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "UsePAM" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "X11Forwarding" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "AllowUsers" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "$iptoallow" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "PubkeyAuthentication" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+grep -v "PasswordAuthentication" $sshconfig > $sshconfignew && mv $sshconfignew $sshconfig
+
+#append new values to sshd_config
+echo $ssh_port >> $sshconfig
+echo $ssh_permitroot >> $sshconfig
+echo $ssh_kbd >> $sshconfig
+echo $ssh_pam >> $sshconfig
+echo $ssh_x11 >> $sshconfig
+echo $ssh_pbkey >> $sshconfig
+echo $ssh_passwdauth >> $sshconfig
+
+#cleaning up
+rm $sshconfignew
 
 #OPTIONAL usertoallow or iptoallow variables
 if [ -z "$usertoallow" ]
@@ -62,9 +77,16 @@ else
     fi
 fi
 
+#/etc/login.defs
+sed "s/022/$umasknum/" /etc/login.defs > /etc/login.defs.new
+echo $minrounds >> /etc/login.defs.new
+echo $maxrounds >> /etc/login.defs.new
+cp /etc/login.defs.new /etc/login.defs && rm /etc/login.defs.new
 
 #install fail2ban then delete default or exsisting config from this script
+fail2banconfig="sshd.conf"
 apt install fail2ban -y && cd /etc/fail2ban/jail.d/ && rm defaults-debian.conf || rm $fail2banconfig
+
 #add variables to config
 echo "[sshd]" >> $fail2banconfig
 echo enabled=$enabled >> $fail2banconfig
